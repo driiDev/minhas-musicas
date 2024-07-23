2fc018c9cf8938772cd2e5b10d98cc81613a49e8
Andrielle Soares <andriellesoares76@gmail.com>
2024-07-23 16:33:00 -0300

    first commit

---
+++ src/br/com/andrielle/minhasmusicas/principal/Principal.java
@@ -0,0 +1,35 @@
+package br.com.andrielle.minhasmusicas.principal;
+
+import br.com.andrielle.minhasmusicas.modelos.MinhasPreferidas;
+import br.com.andrielle.minhasmusicas.modelos.Musica;
+import br.com.andrielle.minhasmusicas.modelos.Podcast;
+
+public class Principal {
+    public static void main(String[] args) {
+        Musica minhaMusica = new Musica();
+        minhaMusica.setTitulo("Treat you better");
+        minhaMusica.setCantor("Shawn Mendes");
+
+        for (int i = 0; i < 1000; i++) {
+            minhaMusica.reproduz();
+        }
+        for (int i = 0; i < 50; i++) {
+            minhaMusica.curte();
+        }
+        Podcast meuPodcast = new Podcast();
+        meuPodcast.setTitulo("BolhaDev");
+        meuPodcast.setApresentador("Marcus Mendes");
+
+        for (int i = 0; i < 5000; i++) {
+            meuPodcast.reproduz();
+        }
+        for (int i = 0; i < 1000; i++) {
+            meuPodcast.curte();
+        }
+
+        MinhasPreferidas preferidas = new MinhasPreferidas();
+        preferidas.inclui(meuPodcast);
+        preferidas.inclui(minhaMusica);
+
+    }
+}
